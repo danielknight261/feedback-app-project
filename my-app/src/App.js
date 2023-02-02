40bf62b9936a7428e5e9b907f627336bf307f321
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-02-02 15:45:59 +0000

    header build

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Feedback Application</h1>
+      </div>
+    </>
+  );
+}
+
+
+
+export default App;
