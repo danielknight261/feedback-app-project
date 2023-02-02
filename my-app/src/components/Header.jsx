158aa30e82dd8e9d0043b9380dec67ba7a045627
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-02-02 16:05:05 +0000

    style changes using Header.proptypes

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types'
 
-function Header({text}) {
+
+
+function Header({text, bgColor, textColor}) {
+
+    const headerStyles = {
+        backgroundColor: bgColor, 
+        color: textColor,
+    }
+
   return (
-    <header>
+    <header style={headerStyles}>
     <div className="container">
         <h1>{text}</h1>
     </div>
@@ -13,11 +21,17 @@ function Header({text}) {
 
   Header.defaultProps = {
     text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6801'
 }
 
 Header.propTypes = {
     text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
 }
+
+
 
 
 
