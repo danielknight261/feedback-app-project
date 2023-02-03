e3fc9f894f8e550a5b9b4b6d32a707f501bb0c98
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-02-03 13:45:45 +0000

    updates

--- my-app/src/components/FeedbackItem.jsx
+++ my-app/src/components/FeedbackItem.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react"
+import {FaTimes} from 'react-icons/fa'
+import Card from './shared/Card'
 
-function FeedbackItem({item}) {
+
+function FeedbackItem({item, handleDelete}) {
+  
   
 return (
-    <div className="card">
+    <Card>
         <div className="num-display">{item.rating}</div>
-        <div className="text-display">{item.text}</div>
-       
-    </div>
+        <button onClick={() => handleDelete(item.id)} 
+        className="close">
+          <FaTimes color='purple'/>
+        </button>
+        <div className="text-display">{item.text}</div> 
+    </Card>
   )
 }
 
